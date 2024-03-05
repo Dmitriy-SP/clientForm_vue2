8185d421c57da1dc84e628c0016247f5376c1902
dmitriy <dmitriychereha@gmail.com>
2024-03-05 21:09:28 +0300

    deploy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import './assets/main.css'
+import "./assets/main.css";
+
+Vue.config.devtools = true;
 
 new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
